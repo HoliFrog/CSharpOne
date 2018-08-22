923bce472975ac5caac57dbaa632e88648c65896
EDG\olivier.menuel <olivier.menuel@campus-numerique-in-the-alps.com>
2018-08-22 10:34:54 +0200

    finish test with mock

--- BonjourTest/BonjourTest1.cs
+++ BonjourTest/BonjourTest1.cs
@@ -1,25 +1,52 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bonjour;
+using NMock;
 
 namespace BonjourTest
 {
     [TestClass]
     public class BonjourTest1
     {
+        private MockFactory _mockFactory;
+        private Mock<IDateTime> _mockIDateTime;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockFactory = new MockFactory();
+            _mockIDateTime = _mockFactory.CreateMock<IDateTime>();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _mockFactory.VerifyAllExpectationsHaveBeenMet();
+            _mockFactory.Dispose();
+        }
 
         [TestMethod]
         public void TestBonjour()
         {
-
             FakeDate fakeDate = new FakeDate(2018, 8, 21, 10, 0, 0);
-
             Message target = new Message(fakeDate, 9, 13, 18);
             string excpected = "Bonjour";
             string message = target.DisplayDayMessage();
             Assert.IsTrue(message.Contains(excpected));
             Assert.IsTrue(message.Contains("mardi"));
         }
+
+        [TestMethod]
+        public void TestMockBonjour()
+        {
+            _mockIDateTime.Expects.Any.GetProperty(_ => _.date).WillReturn(new DateTime(2018, 8, 21, 10, 0, 0));
+            Message target = new Message(_mockIDateTime.MockObject, 9, 13, 18);
+            string excpected = "Bonjour";
+            string message = target.DisplayDayMessage();
+            Assert.IsTrue(message.Contains(excpected));
+            Assert.IsTrue(message.Contains("mardi"));
+        }
+
         [TestMethod]
         public void TestBonAprem()
         {
